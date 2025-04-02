a43449b238bfce849514c5e5dc8e7735c9542df3
Md Danish Ansari <m.danishansari004@gmail.com>
2025-04-02 20:22:27 +0530

    Exploring CRUD operations

--- src/app.js
+++ src/app.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.delete('/user', (req,res)=>{
+    res.send("Deleted successfully");
+})
+app.get('/user', (req,res)=>{
+    res.send({firstname: "Md Danish", lastname: "Ansari"});
+})
+
+app.post('/user', (req, res)=>{
+    
+    res.send("Data successfully daved to the database");
+})
+
+
+
 app.get('/contact', (req, res) => {
   res.send('Phone number')
 });
@@ -9,6 +23,8 @@ app.get('/contact', (req, res) => {
 app.use((req,res)=>{
     res.send("Assalam Alaikum from the server!");
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
