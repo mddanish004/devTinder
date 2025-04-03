54ac36f21d6dd54c3b4cfbe9f65036e3f9b9f403
Md Danish Ansari <m.danishansari004@gmail.com>
2025-04-03 19:40:20 +0530

    Multi-routing

--- src/app.js
+++ src/app.js
@@ -2,30 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.delete('/user', (req,res)=>{
-    res.send("Deleted successfully");
-})
-app.get('/user', (req,res)=>{
-    res.send({firstname: "Md Danish", lastname: "Ansari"});
-})
-
-app.post('/user', (req, res)=>{
-    
-    res.send("Data successfully daved to the database");
-})
-
-
-
-app.get('/contact', (req, res) => {
-  res.send('Phone number')
-});
-
-app.use((req,res)=>{
-    res.send("Assalam Alaikum from the server!");
-})
-
-
-
+app.use('/user',
+   (req,res, next)=>{
+    // res.send("Route handler 1: "); 
+    console.log("Handling the route");
+     
+    next();
+},
+(req,res) =>{
+  console.log("Handling the route 2");
+  res.send("Response 2");
+}
+);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
